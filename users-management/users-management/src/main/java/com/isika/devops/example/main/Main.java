8d240d7f832f8cbe62d09278333e525e401a762c
Stagiaire <Stagiaire@DESKTOP-G68IG82>
2020-09-18 16:38:55 +0200

    Cleaning

--- users-management/users-management/src/main/java/com/isika/devops/example/main/Main.java
+++
@@ -1,9 +0,0 @@
-package com.isika.devops.example.main;
-
-public class Main {
-
-	public static void main(String[] args) {
-		// TODO : faire le programme d'appel ppur l'utilisation du Service users
-	}
-
-}
